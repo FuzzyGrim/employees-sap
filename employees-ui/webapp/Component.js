155dd068f7631756c109d7c5697105ce7e80c74a
FuzzyGrim <stromed_chariotman@aleeas.com>
2024-09-25 09:36:14 +0200

    define app descriptors

--- employees-ui/webapp/Component.js
+++ employees-ui/webapp/Component.js
@@ -1,29 +1,20 @@
-sap.ui.define([
-  "sap/ui/core/UIComponent",
-  "sap/ui/model/json/JSONModel",
-  "sap/ui/model/resource/ResourceModel"
-], (UIComponent, JSONModel, ResourceModel) => {
-  "use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/UIComponent",
+  ],
+  (UIComponent) => {
+    "use strict";
 
-  return UIComponent.extend("ui5.quickstart.Component", {
-     metadata : {
-        "interfaces": ["sap.ui.core.IAsyncContentCreation"],
-        "rootView": {
-           "viewName": "ui5.quickstart.view.App",
-           "type": "XML",
-           "id": "app"
-        }
-     },
+    return UIComponent.extend("ui5.quickstart.Component", {
+      metadata: {
+        interfaces: ["sap.ui.core.IAsyncContentCreation"],
+        manifest: "json",
+      },
 
-     init() {
+      init() {
         // call the init function of the parent
         UIComponent.prototype.init.apply(this, arguments);
-
-        // set i18n model
-        const i18nModel = new ResourceModel({
-           bundleName: "ui5.quickstart.i18n.i18n"
-        });
-        this.setModel(i18nModel, "i18n");
-     }
-  });
-});
+      },
+    });
+  }
+);
